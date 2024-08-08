fb9c86e722764552d4379be6fa9c64739694b6f1
Etho Ledroy <128100160+e-tho@users.noreply.github.com>
2024-08-09 00:10:31 +0200

    Add disconnect feature when selecting the connected network

--- src/app.rs
+++ src/app.rs
@@ -27,44 +27,60 @@ impl App {
     }
 
     pub async fn run(&mut self, menu: Menu) -> Result<Option<String>> {
-        if let Some(ssid) = menu
-            .select_ssid(&mut self.station, self.log_sender.clone())
-            .await?
-        {
-            let (network, _) = self
-                .station
-                .new_networks
-                .iter()
-                .chain(self.station.known_networks.iter())
-                .find(|(network, _)| network.name == ssid)
-                .unwrap();
+        loop {
+            if let Some(ssid) = menu
+                .select_ssid(&mut self.station, self.log_sender.clone())
+                .await?
+            {
+                let (network, _) = self
+                    .station
+                    .new_networks
+                    .iter()
+                    .chain(self.station.known_networks.iter())
+                    .find(|(network, _)| network.name == ssid)
+                    .unwrap();
 
-            if let Some(known_network) = &network.known_network {
-                if known_network.is_autoconnect {
+                if self
+                    .station
+                    .connected_network
+                    .as_ref()
+                    .map_or(false, |cn| cn.name == ssid)
+                {
                     self.log_sender
-                        .send(format!(
-                            "Auto-connecting to known network: {}",
-                            network.name
-                        ))
+                        .send(format!("Disconnecting from network: {}", ssid))
                         .unwrap_or_else(|err| println!("Failed to send message: {}", err));
-                    network.connect(self.log_sender.clone()).await?;
-                    return Ok(Some(ssid));
+                    self.station.disconnect(self.log_sender.clone()).await?;
+                    self.station.refresh().await?;
+                    continue; // Refresh the menu after disconnection
                 }
-            }
 
-            if let Some(passphrase) = menu.prompt_passphrase(&network.name) {
-                self.agent_manager.send_passkey(passphrase)?;
+                if let Some(known_network) = &network.known_network {
+                    if known_network.is_autoconnect {
+                        self.log_sender
+                            .send(format!(
+                                "Auto-connecting to known network: {}",
+                                network.name
+                            ))
+                            .unwrap_or_else(|err| println!("Failed to send message: {}", err));
+                        network.connect(self.log_sender.clone()).await?;
+                        return Ok(Some(ssid));
+                    }
+                }
+
+                if let Some(passphrase) = menu.prompt_passphrase(&network.name) {
+                    self.agent_manager.send_passkey(passphrase)?;
+                } else {
+                    self.agent_manager.cancel_auth()?;
+                }
+
+                network.connect(self.log_sender.clone()).await?;
+                return Ok(Some(ssid));
             } else {
-                self.agent_manager.cancel_auth()?;
+                self.log_sender
+                    .send("No network selected".to_string())
+                    .unwrap_or_else(|err| println!("Failed to send message: {}", err));
+                return Ok(None);
             }
-
-            network.connect(self.log_sender.clone()).await?;
-            return Ok(Some(ssid));
-        } else {
-            self.log_sender
-                .send("No network selected".to_string())
-                .unwrap_or_else(|err| println!("Failed to send message: {}", err));
-            return Ok(None);
         }
     }
 }
