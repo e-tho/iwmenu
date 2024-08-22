23a7f7ab85ef77997d615784d11d66bf7d9e7a3c
Etho Ledroy <128100160+e-tho@users.noreply.github.com>
2024-08-22 16:36:47 +0200

    Add prompt to turn on the device if it's off when the program starts

--- src/app.rs
+++ src/app.rs
@@ -3,13 +3,13 @@ use std::sync::Arc;
 use tokio::sync::mpsc::UnboundedSender;
 
 use crate::{
-    iw::{agent::AgentManager, known_network::KnownNetwork, station::Station},
+    iw::{adapter::Adapter, agent::AgentManager, known_network::KnownNetwork, station::Station},
     menu::Menu,
     notification::NotificationManager,
 };
 
 pub struct App {
-    station: Station,
+    adapter: Adapter,
     agent_manager: AgentManager,
     log_sender: UnboundedSender<String>,
     notification_manager: Arc<NotificationManager>,
@@ -24,10 +24,14 @@ impl App {
         let agent_manager = AgentManager::new().await?;
         let session = agent_manager.session();
 
-        let station = Station::new(session.clone()).await?;
+        let mut adapter = Adapter::new(session.clone(), log_sender.clone()).await?;
+
+        if !adapter.device.is_powered {
+            adapter.device.power_on().await?;
+        }
 
         Ok(Self {
-            station,
+            adapter,
             agent_manager,
             log_sender,
             notification_manager,
@@ -41,58 +45,104 @@ impl App {
         icon_type: &str,
         spaces: usize,
     ) -> Result<Option<String>> {
+        if !self.adapter.device.is_powered {
+            self.offer_to_enable_adapter(menu, menu_command, icon_type, spaces)
+                .await?;
+            self.adapter.refresh(self.log_sender.clone()).await?;
+        }
+
         loop {
-            if let Some(output) = menu
-                .show_menu(menu_command, &mut self.station, icon_type, spaces)
-                .await?
-            {
-                if output.contains("Scan") {
-                    self.handle_scan().await?;
-                } else if output.contains("Known Networks") {
-                    if let Some(known_network) = menu
-                        .show_known_networks_menu(
-                            menu_command,
-                            &mut self.station,
-                            icon_type,
-                            spaces,
-                        )
+            if let Some(station) = self.adapter.device.station.as_mut() {
+                let output = menu
+                    .show_menu(menu_command, station, icon_type, spaces)
+                    .await?;
+
+                if let Some(output) = output {
+                    if output.contains("Scan") {
+                        self.handle_scan().await?;
+                    } else if output.contains("Known Networks") {
+                        if let Some(known_network) = menu
+                            .show_known_networks_menu(menu_command, station, icon_type, spaces)
+                            .await?
+                        {
+                            self.handle_known_network_options(
+                                menu,
+                                menu_command,
+                                &known_network,
+                                icon_type,
+                                spaces,
+                            )
+                            .await?;
+                        }
+                    } else if output.contains("Settings") {
+                        self.handle_settings(menu, menu_command, icon_type, spaces)
+                            .await?;
+                    } else if let Some(ssid) = self
+                        .handle_network_selection(menu, menu_command, &output, icon_type, spaces)
                         .await?
                     {
-                        self.handle_known_network_options(
-                            menu,
-                            menu_command,
-                            &known_network,
-                            icon_type,
-                            spaces,
-                        )
-                        .await?;
+                        return Ok(Some(ssid));
                     }
-                } else if output.contains("Settings") {
-                    self.handle_settings(menu, menu_command, icon_type, spaces)
-                        .await?;
-                } else if let Some(ssid) = self
-                    .handle_network_selection(menu, menu_command, &output, icon_type, spaces)
-                    .await?
-                {
-                    return Ok(Some(ssid));
+                } else {
+                    self.log_sender
+                        .send("No network selected".to_string())
+                        .unwrap_or_else(|err| println!("Failed to send message: {}", err));
+                    return Ok(None);
                 }
             } else {
                 self.log_sender
-                    .send("No network selected".to_string())
+                    .send("No station available".to_string())
                     .unwrap_or_else(|err| println!("Failed to send message: {}", err));
                 return Ok(None);
             }
         }
     }
 
+    async fn offer_to_enable_adapter(
+        &self,
+        menu: &Menu,
+        menu_command: &Option<String>,
+        icon_type: &str,
+        spaces: usize,
+    ) -> Result<()> {
+        if let Some(output) = menu.prompt_enable_adapter(menu_command, icon_type, spaces) {
+            if output.contains("Power On Device") {
+                self.adapter.device.power_on().await?;
+                self.log_sender
+                    .send("Adapter enabled".to_string())
+                    .unwrap_or_else(|err| println!("Failed to send message: {}", err));
+                self.notification_manager.send_notification(
+                    None,
+                    Some("Adapter enabled".to_string()),
+                    None,
+                    Some(notify_rust::Timeout::Milliseconds(3000)),
+                );
+            } else {
+                self.log_sender
+                    .send("Adapter remains disabled".to_string())
+                    .unwrap_or_else(|err| println!("Failed to send message: {}", err));
+                self.notification_manager.send_notification(
+                    None,
+                    Some("Adapter remains disabled".to_string()),
+                    None,
+                    Some(notify_rust::Timeout::Milliseconds(3000)),
+                );
+            }
+        }
+
+        Ok(())
+    }
+
     async fn handle_scan(&mut self) -> Result<()> {
-        self.station
-            .scan(
-                self.log_sender.clone(),
-                Arc::clone(&self.notification_manager),
-            )
-            .await?;
-        self.station.refresh().await?;
+        if let Some(station) = self.adapter.device.station.as_mut() {
+            station
+                .scan(
+                    self.log_sender.clone(),
+                    Arc::clone(&self.notification_manager),
+                )
+                .await?;
+            station.refresh().await?;
+        }
         Ok(())
     }
 
@@ -119,13 +169,17 @@ impl App {
                             self.notification_manager.clone(),
                         )
                         .await?;
-                    self.station.refresh().await?;
+                    if let Some(station) = self.adapter.device.station.as_mut() {
+                        station.refresh().await?;
+                    }
                 }
                 option if option.contains("Forget Network") => {
                     known_network
                         .forget(self.log_sender.clone(), self.notification_manager.clone())
                         .await?;
-                    self.station.refresh().await?;
+                    if let Some(station) = self.adapter.device.station.as_mut() {
+                        station.refresh().await?;
+                    }
                 }
                 _ => {}
             }
@@ -141,69 +195,68 @@ impl App {
         icon_type: &str,
         spaces: usize,
     ) -> Result<Option<String>> {
-        let networks = self
-            .station
-            .new_networks
-            .iter()
-            .chain(self.station.known_networks.iter());
-
-        if let Some((network, _)) =
-            menu.select_network(networks, output.to_string(), icon_type, spaces)
-        {
-            if self
-                .station
-                .connected_network
-                .as_ref()
-                .map_or(false, |cn| cn.name == network.name)
-            {
-                self.station
-                    .disconnect(self.log_sender.clone(), self.notification_manager.clone())
-                    .await?;
-                self.station.refresh().await?;
-                return Ok(None);
-            }
-
-            if self
-                .station
+        if let Some(station) = self.adapter.device.station.as_mut() {
+            let networks = station
                 .new_networks
                 .iter()
-                .any(|(n, _)| n.name == network.name)
-            {
-                self.log_sender
-                    .send(format!("Connecting to new network: {}", network.name))
-                    .unwrap_or_else(|err| println!("Failed to send message: {}", err));
-            }
+                .chain(station.known_networks.iter());
 
-            if let Some(known_network) = &network.known_network {
-                if known_network.is_autoconnect {
-                    self.log_sender
-                        .send(format!(
-                            "Auto-connecting to known network: {}",
-                            network.name
-                        ))
-                        .unwrap_or_else(|err| println!("Failed to send message: {}", err));
-                    network
-                        .connect(self.log_sender.clone(), self.notification_manager.clone())
+            if let Some((network, _)) =
+                menu.select_network(networks, output.to_string(), icon_type, spaces)
+            {
+                if station
+                    .connected_network
+                    .as_ref()
+                    .map_or(false, |cn| cn.name == network.name)
+                {
+                    station
+                        .disconnect(self.log_sender.clone(), self.notification_manager.clone())
                         .await?;
-                    return Ok(Some(network.name.clone()));
+                    station.refresh().await?;
+                    return Ok(None);
                 }
-            }
 
-            if let Some(passphrase) = menu.prompt_passphrase(menu_command, &network.name, icon_type)
-            {
-                self.agent_manager.send_passkey(passphrase)?;
-            } else {
-                self.agent_manager.cancel_auth()?;
-            }
+                if station
+                    .new_networks
+                    .iter()
+                    .any(|(n, _)| n.name == network.name)
+                {
+                    self.log_sender
+                        .send(format!("Connecting to new network: {}", network.name))
+                        .unwrap_or_else(|err| println!("Failed to send message: {}", err));
+                }
 
-            network
-                .connect(self.log_sender.clone(), self.notification_manager.clone())
-                .await?;
-            self.station.refresh().await?;
-            Ok(Some(network.name.clone()))
-        } else {
-            Ok(None)
+                if let Some(known_network) = &network.known_network {
+                    if known_network.is_autoconnect {
+                        self.log_sender
+                            .send(format!(
+                                "Auto-connecting to known network: {}",
+                                network.name
+                            ))
+                            .unwrap_or_else(|err| println!("Failed to send message: {}", err));
+                        network
+                            .connect(self.log_sender.clone(), self.notification_manager.clone())
+                            .await?;
+                        return Ok(Some(network.name.clone()));
+                    }
+                }
+
+                if let Some(passphrase) =
+                    menu.prompt_passphrase(menu_command, &network.name, icon_type)
+                {
+                    self.agent_manager.send_passkey(passphrase)?;
+                } else {
+                    self.agent_manager.cancel_auth()?;
+                }
+
+                network
+                    .connect(self.log_sender.clone(), self.notification_manager.clone())
+                    .await?;
+                station.refresh().await?;
+                return Ok(Some(network.name.clone()));
+            }
         }
+        Ok(None)
     }
 
     async fn handle_settings(
@@ -225,28 +278,16 @@ impl App {
     }
 
     async fn disable_adapter(&self) -> Result<()> {
-        if let Some(adapter) = self.station.session.adapter() {
-            adapter.set_power(false).await?;
-            self.log_sender
-                .send("Adapter disabled".to_string())
-                .unwrap_or_else(|err| println!("Failed to send message: {}", err));
-            self.notification_manager.send_notification(
-                None,
-                Some("Adapter disabled".to_string()),
-                None,
-                Some(notify_rust::Timeout::Milliseconds(3000)),
-            );
-        } else {
-            self.log_sender
-                .send("No adapter found to disable".to_string())
-                .unwrap_or_else(|err| println!("Failed to send message: {}", err));
-            self.notification_manager.send_notification(
-                None,
-                Some("No adapter found to disable".to_string()),
-                None,
-                Some(notify_rust::Timeout::Milliseconds(3000)),
-            );
-        }
+        self.adapter.device.power_off().await?;
+        self.log_sender
+            .send("Adapter disabled".to_string())
+            .unwrap_or_else(|err| println!("Failed to send message: {}", err));
+        self.notification_manager.send_notification(
+            None,
+            Some("Adapter disabled".to_string()),
+            None,
+            Some(notify_rust::Timeout::Milliseconds(3000)),
+        );
         Ok(())
     }
 }
